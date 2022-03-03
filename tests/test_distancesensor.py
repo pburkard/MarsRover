e436c776214f6385910852d08f8aa2fef1646ae7
pburkard <pascal.burkard@hotmail.com>
2022-03-03 14:19:51 +0100

    delete file - distancesensor test is merged to test_sensor

--- tests/test_distancesensor.py
+++
@@ -1,8 +0,0 @@
-from marsrovercore.modules.distance_vl53l0x import DistanceSensor
-from marsrovercore.modules.gpio import GPIO
-
-def test_distance():
-    gpio = GPIO()
-    distancesensor = DistanceSensor()
-    result = distancesensor.get_distance()
-    assert result > 0
